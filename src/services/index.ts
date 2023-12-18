ed4ba6785822fbca309a8f32a2984870dfd450af
Carlos Vinicius <viniprograming@gmail.com>
2023-12-18 02:09:16 -0300

    Testing

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "data",
+  baseURL: "https://vote-application-worldrag.vercel.app",//process.env.NEXT_PUBLIC_API_URL || "data",
   timeout: 1000 * 60 * 15,
   headers: {
     'Content-Type': 'application/json',
